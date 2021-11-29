4e7624792f0f9016354af9eea35a516c84e9a1da
AndiZirahitaf <andikafatihariz@gmail.com>
2021-11-29 13:08:30 +0700

    Rifqi

---
+++ app/src/main/java/learn/idn/tokobisa/MainActivity.kt
@@ -0,0 +1,11 @@
+package learn.idn.tokobisa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
